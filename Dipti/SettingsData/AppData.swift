727e08ba10a6606f524b322ca93eb708e4efa1b6
Hadi Sharghi <hsharghi@icloud.com>
2020-01-20 21:28:41 +0330

    add default app colors

--- Dipti/SettingsData/AppData.swift
+++ Dipti/SettingsData/AppData.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2020 Hadi Sharghi. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 final class AppData {
     
@@ -14,5 +14,12 @@ final class AppData {
     static let loginStatusNotificationKey = "loginStatusNotificationKey"
     static let sideMenuSelectNotificationKey = "sideMenuSelectNotificationKey"
     
+    
+    struct color {
+        static let yellow = UIColor(hexString: "F7C200")
+        static let gray = UIColor(hexString: "D8D8D8")
+        static let lightGray = UIColor(hexString: "F3F4F6")
+        static let darkBlue = UIColor(hexString: "515C6F")
+    }
     
 }
