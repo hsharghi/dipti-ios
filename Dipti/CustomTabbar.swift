6543bfb16f10f5b46f71e7367d9b72ff1c61c707
Hadi Sharghi <hsharghi@icloud.com>
2020-01-20 21:49:14 +0330

    custom tabbar wip

--- Dipti/CustomTabbar.swift
+++ Dipti/CustomTabbar.swift
@@ -11,25 +11,38 @@ import UIKit
 class CustomTabbar: UIViewController {
     
     @IBOutlet var tabbarButtons: [UIButton]!
+    let highlighterView = UIView(frame: .zero)
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupHighlighter()
+        
+    }
     
+    func setupHighlighter() {
+        highlighterView.backgroundColor = AppData.color.yellow
+        highlighterView.isUserInteractionEnabled = false
+        self.view.addSubview(highlighterView)
+        highlighterView.layer.zPosition = 0
+
+    }
     
     @IBAction func tabbarButtonTapped(_ sender: Any) {
         
         let button = sender as! UIButton
         let index = button.tag
         print("button \(index) tapped")
-        
+        highlightButton(for: index)
         
         
     }
     
     private func highlightButton(for index: Int) {
         
-        for button in tabbarButtons {
-            button.alpha = button.tag == index ? 1 : 0
+        if let selectedButton = tabbarButtons.filter({$0.tag == index}).first {
+            highlighterView.frame = selectedButton.frame
         }
-        
     }
     
 }
