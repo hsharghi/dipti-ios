608ee1227891dd714c4a50a91cf9637ec7274d9d
Hadi Sharghi <hsharghi@icloud.com>
2020-01-20 21:28:32 +0330

    add UIColor extension

---
+++ Dipti/Extensions/UIColor+Extensions.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+public extension UIColor {
+    convenience init(rgbaValue: UInt32) {
+        let red   = CGFloat((rgbaValue >> 24) & 0xff) / 255.0
+        let green = CGFloat((rgbaValue >> 16) & 0xff) / 255.0
+        let blue  = CGFloat((rgbaValue >>  8) & 0xff) / 255.0
+        let alpha = CGFloat((rgbaValue      ) & 0xff) / 255.0
+
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+
+    convenience init(hexString: String) {
+        let hex = hexString.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
+        var int = UInt64()
+        Scanner(string: hex).scanHexInt64(&int)
+        let a, r, g, b: UInt64
+        switch hex.count {
+        case 3: // RGB (12-bit)
+            (r, g, b, a) = ((int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17, 255)
+        case 6: // RGB (24-bit)
+            (r, g, b, a) = (int >> 16, int >> 8 & 0xFF, int & 0xFF, 255)
+        case 8: // ARGB (32-bit)
+            (r, g, b, a) = (int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF, int >> 24)
+        default:
+            (r, g, b, a) = (0, 0, 0, 255)
+        }
+        self.init(red: CGFloat(r) / 255, green: CGFloat(g) / 255, blue: CGFloat(b) / 255, alpha: CGFloat(a) / 255)
+    }
+
+}
