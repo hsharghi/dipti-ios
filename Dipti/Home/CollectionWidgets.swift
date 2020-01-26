117e13c8127a3ab492a1421b7f4b1e7d11f8aee4
Hadi Sharghi <hsharghi@icloud.com>
2020-01-26 15:34:25 +0330

    using cache manager for widgets

--- Dipti/Home/CollectionWidgets.swift
+++ Dipti/Home/CollectionWidgets.swift
@@ -16,16 +16,27 @@ class CollectionFullWidthWidget: UIViewController, UIImageViewObserverDelegate {
     @IBOutlet weak var collectionLabel: UILabel!
     @IBOutlet weak var infoLabel: UILabel!
     
+    var backgroundImage: UIImage?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         viewAllButton.rounded(color: .clear, width: 1, paddingX: 0, paddingY: 0)
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
         backgroundImageView.delegate = self
+        backgroundImageView.image = backgroundImage
     }
     
     func imageDidSet(image: UIImage?, colors: UIImageColors?) {
-        
-        AppHelper.addGradientLayer(to: backgroundImageView, startColor: colors?.background ?? .clear, endColor: .clear, startPoint: CGPoint(x: 0, y: 0.5), endPoint: CGPoint(x: 0.5, y: 0.5))
+            AppHelper.addGradientLayer(to: self.backgroundImageView,
+                                       startColor: colors?.background ?? .clear,
+                                       endColor: .clear,
+                                       startPoint: CGPoint(x: 0, y: 0.5),
+                                       endPoint: CGPoint(x: 0.8, y: 0.5))
     }
     
     
@@ -53,17 +64,33 @@ protocol UIImageViewObserverDelegate: class {
     func imageDidSet(image: UIImage?, colors: UIImageColors?)
 }
 
+
+import Cachy
+
 class ObservableImageView: UIImageView {
     
     weak var delegate: UIImageViewObserverDelegate?
+    var imageFile: UIImage?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        if let imageFile = imageFile {
+            self.image = imageFile
+        }
+    }
     
     override var image: UIImage? {
         didSet {
             super.image = image
-            image?.getColors({ (colors) in
+            if let key = image?.md5,
+                let colors: UIImageColors = AppData.cacheManager.get(key: key, as: UIImageColors.self, { () -> UIImageColors? in
+                    self.image?.getColors()
+                })
+            {
                 self.delegate?.imageDidSet(image: self.image, colors: colors)
-            })
+                return
+            }
         }
     }
-    
 }
