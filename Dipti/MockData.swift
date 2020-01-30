837cdae4f0e5335fc61c431037fc64ce39977481
Hadi Sharghi <hsharghi@icloud.com>
2020-01-30 15:13:34 +0330

    add products array

--- Dipti/MockData.swift
+++ Dipti/MockData.swift
@@ -29,16 +29,16 @@ class Product {
     }()
     
     static let mockedProducts = [
-        Product(image: "fake-product-01", title: "دامن صورتی چپ و راستی", description: "", designer: "کوکیاژ", price: 0, category: .categoryWomenClothSkirt),
-        Product(image: "fake-product-02", title: "دامن آبی کاربنی پیلی دار", description: "", designer: "لیدا نوبا", price: 0, category: .categoryWomenClothSkirt),
-        Product(image: "fake-product-03", title: "دامن زرد پیلی دار", description: "", designer: "نیلوفر جودت", price: 0, category: .categoryWomenClothSkirt),
-        Product(image: "fake-product-04", title: "جلیقه پافر سرمه ای", description: "", designer: "ای ۸", price: 0, category: .categoryMenClothSliker),
-        Product(image: "fake-product-05", title: "بارانی جلو بسته آبی", description: "", designer: "اِنکانتو", price: 0, category: .categoryMenClothSliker),
-        Product(image: "fake-product-06", title: "نيم بوت سرمه ای لنگج", description: "کفش های دست ساز ژین بر اساس سفارش شما درست می شوند و به همین دلیل ممکن است ۲۰ روز کاری برای آماده کردن آن ها زمان نیاز باشد.در صورت مشکل در سایز،کفش برای سایز مناسب قابل تغییر است.", designer: "ژین", price: 0, category: .categoryMenShoe),
-        Product(image: "fake-product-07", title: "بوت کردی و جوراب ۲", description: "کفش های دست ساز ژین بر اساس سفارش شما درست می شوند و به همین دلیل ممکن است ۲۰ روز کاری برای آماده کردن آن ها زمان نیاز باشد.در صورت مشکل در سایز،کفش برای سایز مناسب قابل تغییر است.", designer: "ژین", price: 0, category: .categoryMenShoe),
-        Product(image: "fake-product-08", title: "کفش پاشنه بلند زرد", description: "کفش های دست ساز آزالی، بر اساس سفارش شما درست می شوند و به همین دلیل ممکن است مدت ۱۰ تا ۱۴ روز برای آماده کردن آن ها زمان نیاز باشد. در صورت مشکل در سایز، کفش برای سایز مناسب قابل تغییر است.", designer: "آزالی", price: 0, category: .categoryWomenShoe),
-        Product(image: "fake-product-09", title: "پاشنه بلند اُپارت", description: "کفش های دست ساز ژین، بر اساس سفارش شما درست می شوند و به همین دلیل ممکن است مدت ۲۰ روز برای آماده کردن آن ها زمان نیاز باشد. در صورت مشکل در سایز، کفش برای سایز مناسب قابل تغییر است.", designer: "ژین", price: 0, category: .categoryWomenShoe),        
-        Product(image: "fake-product-10", title: "پاشنه بلند دُرسای یقه‌دار", description: "کفش های دست ساز ژین، بر اساس سفارش شما درست می شوند و به همین دلیل ممکن است مدت ۲۰ روز برای آماده کردن آن ها زمان نیاز باشد. در صورت مشکل در سایز، کفش برای سایز مناسب قابل تغییر است.", designer: "ژین", price: 0, category: .categoryWomenShoe),
+        Product(image: "fake-product-01", title: "دامن صورتی چپ و راستی", description: "", designer: "کوکیاژ", price: 870000, category: .categoryWomenClothSkirt),
+        Product(image: "fake-product-02", title: "دامن آبی کاربنی پیلی دار", description: "", designer: "لیدا نوبا", price: 1690000, category: .categoryWomenClothSkirt),
+        Product(image: "fake-product-03", title: "دامن زرد پیلی دار", description: "", designer: "نیلوفر جودت", price: 1260000, category: .categoryWomenClothSkirt),
+        Product(image: "fake-product-04", title: "جلیقه پافر سرمه ای", description: "", designer: "ای ۸", price: 990000, category: .categoryMenClothSliker),
+        Product(image: "fake-product-05", title: "بارانی جلو بسته آبی", description: "", designer: "اِنکانتو", price: 1590000, category: .categoryMenClothSliker),
+        Product(image: "fake-product-06", title: "نيم بوت سرمه ای لنگج", description: "کفش های دست ساز ژین بر اساس سفارش شما درست می شوند و به همین دلیل ممکن است ۲۰ روز کاری برای آماده کردن آن ها زمان نیاز باشد.در صورت مشکل در سایز،کفش برای سایز مناسب قابل تغییر است.", designer: "ژین", price: 850000, category: .categoryMenShoe),
+        Product(image: "fake-product-07", title: "بوت کردی و جوراب ۲", description: "کفش های دست ساز ژین بر اساس سفارش شما درست می شوند و به همین دلیل ممکن است ۲۰ روز کاری برای آماده کردن آن ها زمان نیاز باشد.در صورت مشکل در سایز،کفش برای سایز مناسب قابل تغییر است.", designer: "ژین", price: 440000, category: .categoryMenShoe),
+        Product(image: "fake-product-08", title: "کفش پاشنه بلند زرد", description: "کفش های دست ساز آزالی، بر اساس سفارش شما درست می شوند و به همین دلیل ممکن است مدت ۱۰ تا ۱۴ روز برای آماده کردن آن ها زمان نیاز باشد. در صورت مشکل در سایز، کفش برای سایز مناسب قابل تغییر است.", designer: "آزالی", price: 1190000, category: .categoryWomenShoe),
+        Product(image: "fake-product-09", title: "پاشنه بلند اُپارت", description: "کفش های دست ساز ژین، بر اساس سفارش شما درست می شوند و به همین دلیل ممکن است مدت ۲۰ روز برای آماده کردن آن ها زمان نیاز باشد. در صورت مشکل در سایز، کفش برای سایز مناسب قابل تغییر است.", designer: "ژین", price: 1390000, category: .categoryWomenShoe),
+        Product(image: "fake-product-10", title: "پاشنه بلند دُرسای یقه‌دار", description: "کفش های دست ساز ژین، بر اساس سفارش شما درست می شوند و به همین دلیل ممکن است مدت ۲۰ روز برای آماده کردن آن ها زمان نیاز باشد. در صورت مشکل در سایز، کفش برای سایز مناسب قابل تغییر است.", designer: "ژین", price: 955000, category: .categoryWomenShoe),
 
     ]
     
