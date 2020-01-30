392e0f6ebb7c6206fecf56190788747b62d1e7d5
Hadi Sharghi <hsharghi@icloud.com>
2020-01-30 11:28:47 +0330

    add cache package

---
+++ Dipti/SettingsData/CacheKeys.swift
@@ -0,0 +1,13 @@
+//
+//  CacheKeys.swift
+//  Dipti
+//
+//  Created by Hadi Sharghi on 1/23/20.
+//  Copyright © 2020 Hadi Sharghi. All rights reserved.
+//
+
+import UIKit
+
+struct Cache {
+    
+}
