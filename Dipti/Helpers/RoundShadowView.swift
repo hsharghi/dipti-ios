515afe913a74a34e9917ceaa618e8493f796330b
Hadi Sharghi <hsharghi@icloud.com>
2020-04-08 15:47:58 +0430

    add RoundShadowView class to get dropshadow in rounded corner views

---
+++ Dipti/Helpers/RoundShadowView.swift
@@ -0,0 +1,59 @@
+//
+//  RoundView.swift
+//  Dipti
+//
+//  Created by Hadi Sharghi on 4/8/20.
+//  Copyright © 2020 Hadi Sharghi. All rights reserved.
+//
+
+import UIKit
+
+
+
+class RoundShadowView: UIView {
+    
+    let containerView = UIView()
+    let cornerRadius: CGFloat = 20
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        layoutView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        layoutView()
+        
+    }
+    
+    func layoutView() {
+        
+        // set the shadow of the view's layer
+        layer.backgroundColor = UIColor.clear.cgColor
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 1.0)
+        layer.shadowOpacity = 0.4
+        layer.shadowRadius = 6.0
+        
+        // set the cornerRadius of the containerView's layer
+        containerView.layer.cornerRadius = cornerRadius
+        containerView.layer.masksToBounds = true
+        
+        addSubview(containerView)
+        
+        //
+        // add additional views to the containerView here
+        //
+        
+        // add constraints
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        // pin the containerView to the edges to the view
+        containerView.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        containerView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
+        containerView.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        containerView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+}
+
