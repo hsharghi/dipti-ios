a0766431a7a36b4147f821640920222dedaf4a69
Hadi Sharghi <hsharghi@icloud.com>
2020-04-08 13:16:01 +0430

    add extension for CALayerMask

---
+++ Dipti/Extensions/CACornerMask+Extensions.swift
@@ -0,0 +1,33 @@
+//
+//  CAMask+Extensions.swift
+//  Dipti
+//
+//  Created by Hadi Sharghi on 4/8/20.
+//  Copyright © 2020 Hadi Sharghi. All rights reserved.
+//
+
+import UIKit
+
+
+public extension CACornerMask {
+    
+    static var allCorners: CACornerMask {
+        return [.layerMaxXMaxYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMinXMinYCorner]
+    }
+    
+    static var topCorners: CACornerMask {
+        return [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+    }
+    
+    static var bottomCorners: CACornerMask {
+        return [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
+    }
+    
+    static var leftCorners: CACornerMask {
+        return [.layerMinXMaxYCorner, .layerMinXMinYCorner]
+    }
+    
+    static var rightCorners: CACornerMask {
+        return [.layerMaxXMaxYCorner, .layerMaxXMinYCorner]
+    }
+}
