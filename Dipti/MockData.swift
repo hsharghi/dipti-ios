806e1e4a95928673dd1211555f0c56648f7ad0ce
Hadi Sharghi <hsharghi@icloud.com>
2020-05-31 19:55:29 +0430

    add icon to Category properties

--- Dipti/MockData.swift
+++ Dipti/MockData.swift
@@ -71,22 +71,26 @@ class Category: Models, Equatable {
     }
     
     var name: String
+    var icon: UIImage?
     var parent: Category? = nil
     
-    internal init(name: String, category: Category? = nil) {
+    internal init(name: String, parent: Category? = nil, icon: UIImage? = nil) {
         self.name = name
-        self.parent = category
+        self.parent = parent
+        self.icon = icon
     }
     
     static let categoryWomen = Category(name: "زنانه")
     static let categoryMen = Category(name: "مردانه")
-    static let categoryWomenShoe = Category(name: "کفش", category: Category.categoryWomen)
-    static let categoryMenShoe = Category(name: "کفش", category: Category.categoryMen)
-    static let categoryWomenCloth = Category(name: "لباس", category: Category.categoryWomen)
-    static let categoryMenCloth = Category(name: "لباس", category: Category.categoryMen)
-    static let categoryWomenClothSkirt = Category(name: "دامن", category: Category.categoryWomenCloth)
-    static let categoryMenClothSliker = Category(name: "بارانی", category: Category.categoryMenCloth)
-    
+    static let categoryWomenShoe = Category(name: "کفش", parent: Category.categoryWomen, icon: UIImage(named: "category-shoe"))
+    static let categoryMenShoe = Category(name: "کفش", parent: Category.categoryMen)
+    static let categoryWomenCloth = Category(name: "لباس", parent: Category.categoryWomen, icon: UIImage(named: "category-cloth"))
+    static let categoryMenCloth = Category(name: "لباس", parent: Category.categoryMen)
+    static let categoryWomenClothSkirt = Category(name: "دامن", parent: Category.categoryWomenCloth, icon: UIImage(named: "category-cloth"))
+    static let categoryMenClothSliker = Category(name: "بارانی", parent: Category.categoryMenCloth)
+    static let categoryWomenBag = Category(name: "کیف", parent: Category.categoryWomen, icon: UIImage(named: "category-bag"))
+    static let categoryWomenAccessory = Category(name: "اکسسوری", parent: Category.categoryWomen, icon: UIImage(named: "category-accessory"))
+
     static let categories: [Category] = [
         .categoryWomen,
         .categoryWomenShoe, .categoryWomenCloth, .categoryWomenClothSkirt,
