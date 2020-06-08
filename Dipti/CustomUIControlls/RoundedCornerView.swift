7dbd20b4d7ea669f315a6c05e42e452d65468165
Hadi Sharghi <hsharghi@icloud.com>
2020-06-08 14:31:57 +0430

    custom UIView with cornerRadius property

---
+++ Dipti/CustomUIControlls/RoundedCornerView.swift
@@ -0,0 +1,27 @@
+//
+//  RoundedCornerView.swift
+//  Dipti
+//
+//  Created by Hadi Sharghi on 6/6/20.
+//  Copyright © 2020 Hadi Sharghi. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class RoundedCornerView: UIView {
+    
+        
+    /// A UIColor value that determines the color of the bottom line when in the normal state
+    @IBInspectable dynamic open var cornerRadius: CGFloat = 0 {
+        didSet {
+            self.roundConrners(masks: .allCorners, radius: cornerRadius)
+        }
+    }
+
+    
+    override func prepareForInterfaceBuilder() {
+      self.layer.cornerRadius = cornerRadius
+      self.layer.masksToBounds = true
+    }
+    
+}
