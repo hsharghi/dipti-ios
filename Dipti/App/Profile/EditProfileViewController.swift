2380c9efd9002dda742878829a87029be83d1cc9
Hadi Sharghi <hsharghi@icloud.com>
2020-06-17 20:01:12 +0430

    update profile data

---
+++ Dipti/App/Profile/EditProfileViewController.swift
@@ -0,0 +1,72 @@
+//
+//  EditProfileViewController.swift
+//  Dipti
+//
+//  Created by Hadi Sharghi on 6/17/20.
+//  Copyright © 2020 Hadi Sharghi. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: FormViewController {
+    
+    @IBOutlet weak var firstName: TitleBarTextField!
+    @IBOutlet weak var lastName: TitleBarTextField!
+    @IBOutlet weak var phoneNumber: TitleBarTextField!
+    @IBOutlet weak var gender: UISegmentedControl!
+    //    @IBOutlet weak var confirmPassword: SkyFloatingLabelTextField!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupScrollView(scrollView, contentView: contentView)
+        view.viewWithTag(99)?.roundConrners(masks: .allCorners, radius: 8)
+        populateData()
+    }
+
+    
+    private func populateData() {
+        if let customer = AppData.customer {
+            firstName.text = customer.firstName
+            lastName.text = customer.lastName
+            phoneNumber.text = customer.phoneNumber
+            gender.selectedSegmentIndex = customer.gender == "f" ? 0 : 1
+        }
+    }
+    
+
+        @IBAction func saveButtonTapped(_ sender: Any) {
+            guard validateFields() else { return }
+            
+            if let customer = AppData.customer?.with(firstName: firstName.text!, lastName: lastName.text!, gender: gender.selectedSegmentIndex == 0 ? "f" : "m") {
+                flashLoading(for: 1.5) {
+                    CustomerRepository.update(customer: customer)
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
+        
+        
+        private func validateFields() -> Bool {
+            var validation = true
+            
+            if (firstName.text ?? "").isEmpty {
+                firstName.errorMessage = "نام خود را وارد کنید"
+                validation = false
+            }
+            
+            if (lastName.text ?? "").isEmpty {
+                lastName.errorMessage = "نام خانوادگی خود را وارد کنید"
+                validation = false
+            }
+            
+            if let phoneNumber = phoneNumber.text, !phoneNumber.isEmpty, phoneNumber.digits != phoneNumber {
+                self.phoneNumber.errorMessage = "شماره تلفن همراه نامعتبر است"
+                validation = false
+            }
+            
+            return validation
+        }
+        
+}
--- Dipti/App/Profile/ProfileViewController.swift
+++ Dipti/App/Profile/ProfileViewController.swift
@@ -10,13 +10,13 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var nameLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        nameLabel.text = (AppData.customer?.firstName ?? "") + " " + (AppData.customer?.lastName ?? "")
     }
-    
     
     @IBAction func editProfileTapped(_ sender: Any) {
         
@@ -27,7 +27,7 @@ class ProfileViewController: UIViewController {
     }
     
     @IBAction func showAddresses(_ sender: Any) {
-        
+        AppHelper.showAddresses(in: self)
     }
     
     @IBAction func showFavorites(_ sender: Any) {
